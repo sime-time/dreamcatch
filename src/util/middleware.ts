316a3f343612d68b70d3323f01f97cf6ffb3d5dc
Dunn, Simeon <simedunn01@gmail.com>
2024-08-27 14:12:48 -0400

    redo middleware to overcome cloudflare workers limitation

--- src/util/middleware.ts
+++ src/util/middleware.ts
@@ -1,5 +1,18 @@
 import { createMiddleware } from "@solidjs/start/middleware";
-import { authorizePB } from "~/database/pocketbase";
+import PocketBase from "pocketbase";
+import { env } from "~/util/env-helper";
+
+const pb = new PocketBase(env().POCKETBASE_URL);
+
+export const authorizePB = async () => {
+  if (!pb.authStore.isValid) {
+    await pb.admins.authWithPassword(
+      env().POCKETBASE_EMAIL as string,
+      env().POCKETBASE_PASSWORD as string
+    );
+  }
+  return pb;
+}
 
 export default createMiddleware({
   onRequest: async (event) => {
