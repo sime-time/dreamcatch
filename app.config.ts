441f0cb71f3997a45398d74ab6ffd63d8d44a1eb
Dunn, Simeon <simedunn01@gmail.com>
2024-08-27 11:16:37 -0400

    configure cloudflare deployment

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,10 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+    }
+  }
+});
